4a0c937838909fea5cdecc56f021c191e86c663e
LorenzoFramba <lorenzoframba-1@studenti.unitn.it>
2020-12-07 12:45:53 +0100

    added new class cleanData

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from getData import Data
+from cleanData import Clean
 import findspark
 import pandas as pd
 import argparse
@@ -19,26 +20,11 @@ parser = argparse.ArgumentParser()
 def main(config):
 
     findspark.init()
-    df = Data(config).df
+    data = Data(config)
+    spark = data.spark
+    sc = data.sc
 
-    # removing as is stated in the task along with the 'Year'
-    col_to_drop = ['ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn', 'Diverted', 
-               'CarrierDelay', 'WeatherDelay', 'NASDelay', 'SecurityDelay', 'LateAircraftDelay', 'Year']
-    df = df.drop(*col_to_drop)
-    df.show()
-
-    # "CancelationCode" has too much "null" (98% of the data) we will remove it too. Others have no missing values except for "TailNum", that has only 127 values left.  
-    df.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in df.columns]).show()
-
-    # deletion of the "CancelationCode" and droping rows that contain "TailNum"
-    df = df.drop('CancellationCode')
-    df =  df.filter(df.TailNum.isNotNull() )
-
-    df.printSchema()
-
-    # "ArrDelay" and "DepDelay" have string type. We cast them to Integer
-    df = df.withColumn("ArrDelay", df["ArrDelay"].cast(IntegerType()))
-    df = df.withColumn("DepDelay", df["DepDelay"].cast(IntegerType()))
+    df = Clean(config, data.df, spark, sc).df
     df.printSchema()
 
 
