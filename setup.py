7863e41d4f09a742e3584b8e1f39cad1ae749a6a
LorenzoFramba <lorenzoframba-1@studenti.unitn.it>
2020-12-20 14:06:28 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md") as f:
+	long_description = f.read()
+
+setup(
+	name='Prediction',
+	version='1.0',
+	description='Prediction a flight delay with Spark',
+	license='MIT',
+	long_description=long_description,
+	author='Lorenzo Framba',
+
+	install_requires=[
+		'seaborn==0.11.0',
+		'pyspark==3.0.1',
+		'py4j==0.10.9',
+		'plotly==4.5.4',
+		'pandas==1.0.3',
+		'matplotlib==3.1.1',
+		'findspark==1.4.2'
+	],
+	scripts=[
+		'main.py',
+		'trainer.py',
+		'getData.py',
+		'visualization.py',
+		'cleanData.py'
+	]
+
+)
--- visualization.py
+++ visualization.py
@@ -1,6 +1,5 @@
 import random
 import seaborn as sns
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
